cd4dd128c55194fad9a48f8c5f562d957dcc6d23
Mondara <mondara.t@hotmail.com>
2023-09-26 11:42:56 +1000

    Fixed Height Issue

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ThemeContext, { initialThemeState } from "./ThemeContext";
 
 
 const ThemeProvider: React.FC = ({ children }) => {
   const [theme, setTheme] = useState(initialThemeState.theme);
   const toggleTheme = () => {
-    if(theme === 'dark') {
+    if (theme === 'dark') {
       setTheme('light')
     } else {
       setTheme('dark');
     }
   }
-  
+
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }} >
-      <div className={`theme--${theme}`}>{children}</div>
+      <div className={`theme theme--${theme}`}>{children}</div>
     </ThemeContext.Provider>
   );
 };
